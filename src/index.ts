222479c4d39f3e4bdca21c9dd72c824e9a1fe5d6
Nie-Mand <chill.out.x19@gmail.com>
2023-02-01 07:53:43 +0100

    hello world

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import fs from 'fs'
+import express, { ErrorRequestHandler } from 'express'
+import cors from 'cors'
+import { json } from 'body-parser'
+import path from 'path'
+
+const app = express()
+
+app.use(json())
+app.use(cors())
+
+app.get('/songs', async (rq, rs) => {
+  const q = (rq.query.q || '').toString()
+  const base = path.join(__dirname, '../songs/released/', q)
+
+  try {
+    if (rq.query.q) {
+      const song = await fs.readFileSync(base)
+      return rs.send(song)
+    }
+  } catch {}
+
+  try {
+    const list = fs.readdirSync(base)
+    const response = `
+        <h1> Songs:  </h1>
+        <ul>
+            ${list
+              .map(
+                song => `
+                <li> 
+                    <a href="/songs/?q=${song}" >${song}</a>
+                </li>`
+              )
+              .join('')}
+            </ul>
+            `
+    return rs.send(response)
+  } catch {}
+
+    return rs.send('idk')
+})
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000')
+})
