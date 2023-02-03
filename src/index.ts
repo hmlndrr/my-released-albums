e1003798ff94a9a376e1fe4bada2b3fdf1ec714a
Nie-Mand <chill.out.x19@gmail.com>
2023-02-03 02:57:43 +0100

    maybe this was the error?

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import fs from 'fs'
-import express, { ErrorRequestHandler } from 'express'
+import express from 'express'
 import cors from 'cors'
 import { json } from 'body-parser'
-import path from 'path'
+import * as path from 'path'
 
 const app = express()
 
@@ -10,45 +10,48 @@ app.use(json())
 app.use(cors())
 
 app.get('/', async (rq, rs) => {
-  const q = (rq.query.q || '')
-    .toString()
-    .replaceAll('..', '')
-    .split('')
-    .filter(c => c.charCodeAt(0) > 0)
-    .join('')
-
-  const base = path.join(__dirname, '../songs/released/', q)
-
-  console.log(base)
-
   try {
-    if (rq.query.q) {
-      const song = await fs.readFileSync(base)
-      return rs.send(song)
-    }
-  } catch {}
+    const q = (rq.query.q || '')
+      .toString()
+      .replaceAll('..', '')
+      .split('')
+      .filter(c => c.charCodeAt(0) > 0)
+      .join('')
 
-  try {
-    const list = fs.readdirSync(base)
-    const response = `
-        <h1> Songs:  </h1>
-        <ul>
-            ${list
-              .map(
-                song => `
+    const base = path.join(__dirname, '../songs/released/', q)
+
+    try {
+      if (rq.query.q) {
+        const song = await fs.readFileSync(base)
+        return rs.send(song)
+      }
+    } catch {}
+
+    try {
+      const list = fs.readdirSync(base)
+      const response = `
+      <h1> Songs:  </h1>
+      <ul>
+      ${list
+        .map(
+          song => `
                 <li> 
-                    <a href="/?q=${song}" >${song}</a>
+                <a href="/?q=${song}" >${song}</a>
                 </li>`
-              )
-              .join('')}
-            </ul>
-            `
-    return rs.send(response)
-  } catch {}
+        )
+        .join('')}
+                </ul>
+                `
+      return rs.send(response)
+    } catch {}
 
-  return rs.send('idk')
+    return rs.send('idk')
+  } catch {
+    return rs.send('idk')
+  }
 })
 
 app.listen(3000, () => {
   console.log('Server started on port 3000')
 })
+            
